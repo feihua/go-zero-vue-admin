27a409f368b4e43518698e6c1756bca4d83268dc
liufeihua <1002219331@qq.com>
2021-04-05 16:31:17 +0800

    Initial commit

---
+++ src/store/modules/user.js
@@ -0,0 +1,104 @@
+import storage from 'store'
+import { login, getInfo, logout } from '@/api/login'
+import { ACCESS_TOKEN } from '@/store/mutation-types'
+// import { welcome } from '@/utils/util'
+
+const user = {
+  state: {
+    token: '',
+    name: '',
+    welcome: '',
+    avatar: '',
+    roles: [],
+    info: {},
+    menus: []
+  },
+
+  mutations: {
+    SET_TOKEN: (state, token) => {
+      state.token = token
+    },
+    SET_NAME: (state, name) => {
+      state.name = name
+    },
+    SET_AVATAR: (state, avatar) => {
+      state.avatar = avatar
+    },
+    SET_ROLES: (state, roles) => {
+      state.roles = roles
+    },
+    SET_INFO: (state, info) => {
+      state.info = info
+    },
+    SET_MENUS: (state, avatar) => {
+      state.menus = avatar
+    }
+  },
+
+  actions: {
+    // 登录
+    Login ({ commit }, userInfo) {
+      return new Promise((resolve, reject) => {
+        login(userInfo).then(response => {
+          storage.set(ACCESS_TOKEN, response.token, 7 * 24 * 60 * 60 * 1000)
+          commit('SET_TOKEN', response.token)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    // 获取用户信息
+    GetInfo ({ commit }) {
+      return new Promise((resolve, reject) => {
+        getInfo().then(response => {
+          const { name, avatar, menuTreeVue } = response
+
+          // if (result.role && result.role.permissions.length > 0) {
+          //   const role = result.role
+          //   role.permissions = result.role.permissions
+          //   role.permissions.map(per => {
+          //     if (per.actionEntitySet != null && per.actionEntitySet.length > 0) {
+          //       const action = per.actionEntitySet.map(action => { return action.action })
+          //       per.actionList = action
+          //     }
+          //   })
+          //   role.permissionList = role.permissions.map(permission => { return permission.permissionId })
+          //   commit('SET_ROLES', result.role)
+          //   commit('SET_INFO', result)
+          // } else {
+          //   reject(new Error('getInfo: roles must be a non-null array !'))
+          // }
+
+          commit('SET_NAME', name)
+          commit('SET_AVATAR', avatar)
+
+          // commit('SET_MENUS', tree(menuTreeVue,0,'parent_id'))
+          commit('SET_MENUS', menuTreeVue)
+          resolve(menuTreeVue)
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    // 登出
+    Logout ({ commit, state }) {
+      return new Promise((resolve) => {
+        logout(state.token).then(() => {
+          commit('SET_TOKEN', '')
+          commit('SET_ROLES', [])
+          storage.remove(ACCESS_TOKEN)
+          resolve()
+        }).catch(() => {
+          resolve()
+        }).finally(() => {
+        })
+      })
+    }
+
+  }
+}
+
+export default user
